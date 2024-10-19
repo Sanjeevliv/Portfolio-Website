87453305cfb807f3368622dd2868b26490286c31
Sanjeevliv <91120312+Sanjeevliv@users.noreply.github.com>
2024-10-19 18:59:50 +0530

    logout error fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, CustomLogoutView
 
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'), 
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,22 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
+from django.views import *
+from django.shortcuts import redirect
+from django.contrib.auth import logout as auth_logout
+
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url= reverse_lazy('login')
     template_name= 'registration/signup.html'
+
+
+class CustomLogoutView(View):
+    def post(self, request, *args, **kwargs):
+        auth_logout(request) 
+        return redirect('/')
+
+    def get(self, request, *args, **kwargs):
+        return redirect('/')
+    
