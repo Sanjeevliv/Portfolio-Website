b60baefe6850a7879959924bbceaf2f983604cb8
Sanjeevliv <91120312+Sanjeevliv@users.noreply.github.com>
2024-10-19 17:34:32 +0530

    user auth and template view added

--- home/views.py
+++ home/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def HomePageView(response):
-    return HttpResponse('Hello World')
+def HomePageView(request):
+    return render(request,'home.html')
